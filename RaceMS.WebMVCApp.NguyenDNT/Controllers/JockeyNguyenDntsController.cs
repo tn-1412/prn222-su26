e21f1bd2cd0eba11fd54c3ca407df80de6ed488a
tn-1412 <trungnguyen14122005@gmail.com>
2026-05-23 19:47:23 +0700

    Replace controller, add CRUD views, update project refs
    Replaced JockeyNguyenDntController with a new controller using dependency injection and an Index action. Added Razor views for full CRUD operations on jockeys. Updated the project file to include EF Core packages and reference the repositories project.

--- RaceMS.WebMVCApp.NguyenDNT/Controllers/JockeyNguyenDntsController.cs
+++ RaceMS.WebMVCApp.NguyenDNT/Controllers/JockeyNguyenDntsController.cs
@@ -1,5 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RaceMS.Entities.NguyenDNT.Models;
+using RaceMS_Repositories.NguyenDNT.DBContext;
 using RaceMS_Services.NguyenDNT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace RaceMS.WebMVCApp.NguyenDNT.Controllers
 {
@@ -7,17 +15,18 @@ namespace RaceMS.WebMVCApp.NguyenDNT.Controllers
     {
         private readonly IJockeyNguyenDntService jockeyNguyenDntService;
         private readonly IRegistrationNguyenDntService registrationNguyenDntService;
+
         public JockeyNguyenDntController(IJockeyNguyenDntService jockeyNguyenDntService, IRegistrationNguyenDntService registrationNguyenDntService)
         {
             this.jockeyNguyenDntService = jockeyNguyenDntService;
             this.registrationNguyenDntService = registrationNguyenDntService;
         }
+
+        // GET: JockeyNguyenDnt
         public async Task<IActionResult> Index()
         {
             var jockeys = await jockeyNguyenDntService.GetAllAsync();
             return View(jockeys);
-
         }
     }
 }
-
